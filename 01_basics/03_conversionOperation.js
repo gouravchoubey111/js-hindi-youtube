d7d502f8dfa5ca19a1ad7569b3e9da4aa9f33cc0
gouravchoubey111 <73731968+gouravchoubey111@users.noreply.github.com>
2025-02-01 12:34:04 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+// let score = "33"
+
+// console.log(typeof score)
+// // or
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+
+// ---------------------------------------
+
+
+
+// let score = "33"  // 33
+
+// let score = "33abc"  // NaN
+
+// let score = null
+
+// let score = undefined
+
+// let score = true  // 1
+
+// let score = "hitesh"
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// -------------------------------------
+
+
+// let isLoggedIn = 1  // 1 -> true; 0 -> false
+
+// let isLoggedIn = "" // "" -> false
+
+// let isLoggedIn = "hitesh" // "hitesh" -> true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 

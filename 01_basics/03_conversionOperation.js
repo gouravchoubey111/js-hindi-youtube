df1e011d689410da32fdd82a14a9be66d50b0963
gouravchoubey111 <73731968+gouravchoubey111@users.noreply.github.com>
2025-02-01 14:20:00 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -48,5 +48,52 @@
  let stringNumber = String(someNumber)
  console.log(stringNumber);
  console.log(typeof stringNumber);
+
+
+// ************************* Operations *****************************
+
+
+//  let value = 3
+
+//  let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str2 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+console.log(true);  // true
+console.log(+true);  // 1
+console.log(+"");  // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;  // 101
+++gameCounter;  //102
+console.log(gameCounter);  // 102
  
  
